56eeaed6783cd999a56e7bc11991d735d03cd1d3
Tobi823 <Tobi823@users.noreply.github.com>
2022-04-08 14:57:05 +0200

    unit test improved mit junit5

--- ffupdater/src/test/java/de/marmaro/krt/ffupdater/app/impl/BraveIT.kt
+++ ffupdater/src/test/java/de/marmaro/krt/ffupdater/app/impl/BraveIT.kt
@@ -11,13 +11,20 @@ import de.marmaro.krt.ffupdater.app.impl.fetch.github.GithubConsumer.Release
 import de.marmaro.krt.ffupdater.device.ABI
 import io.mockk.*
 import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import kotlinx.coroutines.runBlocking
-import org.junit.*
-import org.junit.Assert.*
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.extension.ExtendWith
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
 import java.io.FileReader
 import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter.ISO_ZONED_DATE_TIME
+import java.util.stream.Stream
 
+@ExtendWith(MockKExtension::class)
 class BraveIT {
     @MockK
     lateinit var context: Context
@@ -30,9 +37,8 @@ class BraveIT {
 
     val packageInfo = PackageInfo()
 
-    @Before
+    @BeforeEach
     fun setUp() {
-        MockKAnnotations.init(this, relaxUnitFun = true)
         every { context.packageManager } returns packageManager
         every { context.getString(R.string.available_version, any()) } returns "/"
         every { packageManager.getPackageInfo(App.BRAVE.detail.packageName, any()) } returns packageInfo
@@ -42,7 +48,20 @@ class BraveIT {
         const val API_URl = "https://api.github.com/repos/brave/brave-browser/releases"
         const val DOWNLOAD_URL = "https://github.com/brave/brave-browser/releases/download"
         const val EXPECTED_VERSION = "1.20.103"
-        val EXPECTED_RELEASE_TIMESTAMP = ZonedDateTime.parse("2021-02-10T11:30:45Z", ISO_ZONED_DATE_TIME)
+        val EXPECTED_RELEASE_TIMESTAMP: ZonedDateTime =
+            ZonedDateTime.parse("2021-02-10T11:30:45Z", ISO_ZONED_DATE_TIME)
+
+        @JvmStatic
+        fun abisWithMetaData(): Stream<Arguments> = Stream.of(
+            Arguments.of(ABI.ARMEABI_V7A, 2, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm.apk", 100446537L),
+            Arguments.of(ABI.ARMEABI_V7A, 3, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm.apk", 100446537L),
+            Arguments.of(ABI.ARM64_V8A, 2, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm64.apk", 171400033L),
+            Arguments.of(ABI.ARM64_V8A, 3, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm64.apk", 171400033L),
+            Arguments.of(ABI.X86, 2, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox86.apk", 131114604L),
+            Arguments.of(ABI.X86, 3, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox86.apk", 131114604L),
+            Arguments.of(ABI.X86_64, 2, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox64.apk", 201926660L),
+            Arguments.of(ABI.X86_64, 3, "$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox64.apk", 201926660L),
+        )
     }
 
     private fun createSut(deviceAbi: ABI): Brave {
@@ -92,207 +111,55 @@ class BraveIT {
         )
     }
 
-    @Test
-    fun `check download info ARMEABI_V7A - 2 network requests required`() {
-        prepareNetworkForReleaseAfterTwoRequests()
-        val result = runBlocking { createSut(ABI.ARMEABI_V7A).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm.apk", result.downloadUrl)
+    @ParameterizedTest(name = "check download info for ABI \"{0}\" - \"{1}\" network requests required")
+    @MethodSource("abisWithMetaData")
+    fun `check download info for ABI X - X network requests required`(
+        abi: ABI,
+        networkRequests: Int,
+        url: String,
+        fileSize: Long,
+    ) {
+        when (networkRequests) {
+            2 -> prepareNetworkForReleaseAfterTwoRequests()
+            3 -> prepareNetworkForReleaseAfterThreeRequests()
+            else -> throw IllegalStateException()
+        }
+        val result = runBlocking { createSut(abi).updateCheck(context) }
+        assertEquals(url, result.downloadUrl)
         assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(100446537L, result.fileSizeBytes)
+        assertEquals(fileSize, result.fileSizeBytes)
         assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
     }
 
-    @Test
-    fun `check download info ARM64_V8A - 2 network requests required`() {
-        prepareNetworkForReleaseAfterTwoRequests()
-        val result = runBlocking { createSut(ABI.ARM64_V8A).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm64.apk", result.downloadUrl)
-        assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(171400033L, result.fileSizeBytes)
-        assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
+    @ParameterizedTest(name = "update check for ABI \"{0}\" - outdated version installed - \"{1}\" network requests required")
+    @MethodSource("abisWithMetaData")
+    fun `update check for ABI X - outdated version installed - X network requests required`(
+        abi: ABI,
+        networkRequests: Int,
+    ) {
+        when (networkRequests) {
+            2 -> prepareNetworkForReleaseAfterTwoRequests()
+            3 -> prepareNetworkForReleaseAfterThreeRequests()
+            else -> throw IllegalStateException()
+        }
+        packageInfo.versionName = "1.18.12"
+        val result = runBlocking { createSut(abi).updateCheck(context) }
+        assertTrue(result.isUpdateAvailable)
     }
 
-    @Test
-    fun `check download info X86 - 2 network requests required`() {
-        prepareNetworkForReleaseAfterTwoRequests()
-        val result = runBlocking { createSut(ABI.X86).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox86.apk", result.downloadUrl)
-        assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(131114604L, result.fileSizeBytes)
-        assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
+    @ParameterizedTest(name = "update check for ABI \"{0}\" - latest version installed - \"{1}\" network requests required")
+    @MethodSource("abisWithMetaData")
+    fun `update check for ABI X - latest version installed - X network requests required`(
+        abi: ABI,
+        networkRequests: Int,
+    ) {
+        when (networkRequests) {
+            2 -> prepareNetworkForReleaseAfterTwoRequests()
+            3 -> prepareNetworkForReleaseAfterThreeRequests()
+            else -> throw IllegalStateException()
+        }
+        packageInfo.versionName = "1.20.103"
+        val result = runBlocking { createSut(abi).updateCheck(context) }
+        assertFalse(result.isUpdateAvailable)
     }
-
-    @Test
-    fun `check download info X86_64 - 2 network requests required`() {
-        prepareNetworkForReleaseAfterTwoRequests()
-        val result = runBlocking { createSut(ABI.X86_64).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox64.apk", result.downloadUrl)
-        assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(201926660L, result.fileSizeBytes)
-        assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
-    }
-
-    @Test
-    fun `check download info ARMEABI_V7A - 3 network requests required`() {
-        prepareNetworkForReleaseAfterThreeRequests()
-        val result = runBlocking { createSut(ABI.ARMEABI_V7A).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm.apk", result.downloadUrl)
-        assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(100446537L, result.fileSizeBytes)
-        assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
-    }
-
-    @Test
-    fun `check download info ARM64_V8A - 3 network requests required`() {
-        prepareNetworkForReleaseAfterThreeRequests()
-        val result = runBlocking { createSut(ABI.ARM64_V8A).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonoarm64.apk", result.downloadUrl)
-        assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(171400033L, result.fileSizeBytes)
-        assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
-    }
-
-    @Test
-    fun `check download info X86 - 3 network requests required`() {
-        prepareNetworkForReleaseAfterThreeRequests()
-        val result = runBlocking { createSut(ABI.X86).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox86.apk", result.downloadUrl)
-        assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(131114604L, result.fileSizeBytes)
-        assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
-    }
-
-    @Test
-    fun `check download info X86_64 - 3 network requests required`() {
-        prepareNetworkForReleaseAfterThreeRequests()
-        val result = runBlocking { createSut(ABI.X86_64).updateCheck(context) }
-        assertEquals("$DOWNLOAD_URL/v$EXPECTED_VERSION/BraveMonox64.apk", result.downloadUrl)
-        assertEquals(EXPECTED_VERSION, result.version)
-        assertEquals(201926660L, result.fileSizeBytes)
-        assertEquals(EXPECTED_RELEASE_TIMESTAMP, result.publishDate)
-    }
-
-//    @Test
-//    fun updateCheck_2releases_updateCheck() {
-//        makeJsonObjectAvailable("latest_contains_NOT_release_version.json", "$API_URl/latest")
-//        makeJsonArrayAvailable(
-//            "2releases_perpage_20_page_1.json",
-//            "$API_URl?per_page=20&page=1"
-//        )
-//
-//        val packageInfo = PackageInfo()
-//        every { packageManager.getPackageInfo(App.BRAVE.detail.packageName, any()) } returns packageInfo
-//
-//        // installed app is up-to-date
-//        runBlocking {
-//            packageInfo.versionName = "1.20.103"
-//            val actual = createSut(ABI.ARMEABI_V7A).updateCheck(context)
-//            assertFalse(actual.isUpdateAvailable)
-//            assertEquals("1.20.103", actual.version)
-//            assertEquals(100446537L, actual.fileSizeBytes)
-//            assertEquals(
-//                ZonedDateTime.parse("2021-02-10T11:30:45Z", ISO_ZONED_DATE_TIME),
-//                actual.publishDate
-//            )
-//        }
-//
-//        // installed app is old
-//        runBlocking {
-//            packageInfo.versionName = "1.18.12"
-//            val actual = createSut(ABI.ARMEABI_V7A).updateCheck(context)
-//            assertTrue(actual.isUpdateAvailable)
-//            assertEquals("1.20.103", actual.version)
-//            assertEquals(100446537L, actual.fileSizeBytes)
-//            assertEquals(
-//                ZonedDateTime.parse("2021-02-10T11:30:45Z", ISO_ZONED_DATE_TIME),
-//                actual.publishDate
-//            )
-//        }
-//    }
-//
-//    @Test
-//    fun updateCheck_3releases_checkDownloadUrlForABI() {
-//        makeJsonObjectAvailable("latest_contains_NOT_release_version.json", "$API_URl/latest")
-//        makeJsonArrayAvailable(
-//            "3releases_perpage_10_page_1.json",
-//            "$API_URl?per_page=20&page=1"
-//        )
-//        makeJsonArrayAvailable(
-//            "3releases_perpage_10_page_2.json",
-//            "$API_URl?per_page=20&page=2"
-//        )
-//
-//        val packageInfo = PackageInfo()
-//        every { packageManager.getPackageInfo(App.BRAVE.detail.packageName, any()) } returns packageInfo
-//
-//        runBlocking {
-//            assertEquals(
-//                "$DOWNLOAD_URL/v1.20.103/BraveMonoarm.apk",
-//                createSut(ABI.ARMEABI_V7A).updateCheck(context).downloadUrl
-//            )
-//        }
-//
-//        runBlocking {
-//            assertEquals(
-//                "$DOWNLOAD_URL/v1.20.103/BraveMonoarm64.apk",
-//                createSut(ABI.ARM64_V8A).updateCheck(context).downloadUrl
-//            )
-//        }
-//
-//        runBlocking {
-//            assertEquals(
-//                "$DOWNLOAD_URL/v1.20.103/BraveMonox86.apk",
-//                createSut(ABI.X86).updateCheck(context).downloadUrl
-//            )
-//        }
-//
-//        runBlocking {
-//            assertEquals(
-//                "$DOWNLOAD_URL/v1.20.103/BraveMonox64.apk",
-//                createSut(ABI.X86_64).updateCheck(context).downloadUrl
-//            )
-//        }
-//    }
-//
-//    @Test
-//    fun updateCheck_3releases_updateCheck() {
-//        makeJsonObjectAvailable("latest_contains_NOT_release_version.json", "$API_URl/latest")
-//        makeJsonArrayAvailable(
-//            "3releases_perpage_10_page_1.json",
-//            "$API_URl?per_page=20&page=1"
-//        )
-//        makeJsonArrayAvailable(
-//            "3releases_perpage_10_page_2.json",
-//            "$API_URl?per_page=20&page=2"
-//        )
-//
-//        val packageInfo = PackageInfo()
-//        every { packageManager.getPackageInfo(App.BRAVE.detail.packageName, any()) } returns packageInfo
-//
-//        // installed app is up-to-date
-//        runBlocking {
-//            packageInfo.versionName = "1.20.103"
-//            val actual = createSut(ABI.ARMEABI_V7A).updateCheck(context)
-//            assertFalse(actual.isUpdateAvailable)
-//            assertEquals("1.20.103", actual.version)
-//            assertEquals(100446537L, actual.fileSizeBytes)
-//            assertEquals(
-//                ZonedDateTime.parse("2021-02-10T11:30:45Z", ISO_ZONED_DATE_TIME),
-//                actual.publishDate
-//            )
-//        }
-//
-//        // installed app is old
-//        runBlocking {
-//            packageInfo.versionName = "1.18.12"
-//            val actual = createSut(ABI.ARMEABI_V7A).updateCheck(context)
-//            assertTrue(actual.isUpdateAvailable)
-//            assertEquals("1.20.103", actual.version)
-//            assertEquals(100446537L, actual.fileSizeBytes)
-//            assertEquals(
-//                ZonedDateTime.parse("2021-02-10T11:30:45Z", ISO_ZONED_DATE_TIME),
-//                actual.publishDate
-//            )
-//        }
-//    }
 }
